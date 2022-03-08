f447329b42220b90f57c69791994f62c633c8746
黄靖翔\小翔 <2038392181@qq.com>
2022-03-08 17:23:13 +0800

    no message

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,73 +1,73 @@
 <template>
-	<div class="home">
-		<div class="bg-moon"></div>
-		<div class="header-background">
-			<!-- 导航 -->
-			<div class="header container">
-				<header-cmp></header-cmp>
-			</div>
-		</div>
-		<wrap></wrap>
-	</div>
+  <div class="home">
+    <div class="bg-moon"></div>
+    <div class="header-background">
+      <!-- 导航 -->
+      <div class="section__hero section__hero-s wf-section">
+        <div class="container-1440">
+          <header-cmp></header-cmp>
+        </div>
+      </div>
+    </div>
+    <wrap></wrap>
+  </div>
 </template>
 
 <script>
-	import headerCmp from "./components/headerCmp.vue";
-	import wrap from "./Harry/wrap.vue";
-	import footerCmp from "./footer/footerCmp.vue";
-	export default {
-		data() {
-			return {};
-		},
-		props: {},
-		computed: {},
-		watch: {},
-		components: {
-			headerCmp,
-			wrap,
-			footerCmp,
-		},
-		created() {
-		},
-		mounted() {
-			//init("home_canvas");
-		},
-		methods: {},
-	};
+import headerCmp from "./components/headerCmp.vue";
+import wrap from "./Harry/wrap.vue";
+import footerCmp from "./footer/footerCmp.vue";
+export default {
+  data() {
+    return {};
+  },
+  props: {},
+  computed: {},
+  watch: {},
+  components: {
+    headerCmp,
+    wrap,
+    footerCmp,
+  },
+  created() {},
+  mounted() {
+    //init("home_canvas");
+  },
+  methods: {},
+};
 </script>
 
 <style scoped lang="less">
+@import "./index";
+@import "../../styles/shiba-ui";
+.bg {
+  position: relative;
+}
+#home_canvas {
+  position: fixed;
+  widows: 100%;
+  height: 100%;
+  z-index: 0;
+}
+@media screen and (min-width: 768px) {
+  .header-background {
+    // background: rgba(33, 21, 59, 0.8);
+  }
 
-	@import "./index";
-	.bg{
-		position: relative;
-	}
-	#home_canvas{
-		position: fixed;
-		widows: 100%;
-		height: 100%;
-		z-index: 0;
-	}
-	@media screen and (min-width: 768px) {
-		.header-background {
-			// background: rgba(33, 21, 59, 0.8);
-		}
+  .bg {
+    // background: url("../../assets/homebgimgpc.png") no-repeat no-repeat;
+    // background-repeat: no-repeat;
+    // background-size: cover;
+    // background-size: 100%;
+  }
+}
 
-		.bg {
-			// background: url("../../assets/homebgimgpc.png") no-repeat no-repeat;
-			// background-repeat: no-repeat;
-			// background-size: cover;
-			// background-size: 100%;
-
-		}
-	}
-
-	@media screen and (max-width: 768px) {
-		.bg {
-			// background: url("../../assets/homebgimgm.png");
-			// background-repeat: no-repeat;
-			// background-size: cover;
-			// background-size: 100%;
-		}
-	}
+@media screen and (max-width: 768px) {
+  .bg {
+    // background: url("../../assets/homebgimgm.png");
+    // background-repeat: no-repeat;
+    // background-size: cover;
+    // background-size: 100%;
+  }
+}
 </style>
